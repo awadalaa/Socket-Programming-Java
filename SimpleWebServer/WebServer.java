e52d0f685db52ce048570ca4487b7a2c1517c542
Alaa Awad <alaa.awad.mail@gmail.com>
2014-01-10 00:20:34 -0500

    part b of WebServer.java serves an httpResponse

--- SimpleWebServer/WebServer.java
+++ SimpleWebServer/WebServer.java
@@ -62,18 +62,106 @@ final class HttpRequest implements Runnable
 		// Display the request line.
 		System.out.println();
 		System.out.println(requestLine);
-
 		// Get and display the header lines.
 		String headerLine = null;
 		while ((headerLine = br.readLine()).length() != 0) {
 			System.out.println(headerLine);
 		}
 		
+		// Extract the filename from the request line.
+		StringTokenizer tokens = new StringTokenizer(requestLine);
+		tokens.nextToken();  // skip over the method, which should be "GET"
+		String fileName = tokens.nextToken();
+
+		// Prepend a "." so that file request is within the current directory.
+		fileName = "." + fileName;
+
+		// Open the requested file.
+		FileInputStream fis = null;
+		boolean fileExists = true;
+		try {
+			fis = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			fileExists = false;
+		}
 		
+		// Construct the response message.
+		String statusLine = null;
+		String contentTypeLine = null;
+		String entityBody = null;
+		if (fileExists) {
+			statusLine = "HTTP/1.1 200 OK" + CRLF;
+			contentTypeLine = "Content-type: " + 
+				contentType( fileName ) + CRLF;
+		} else {
+			statusLine = "HTTP/1.1 404 NOT FOUND";
+			contentTypeLine = "Content-type: text/html" + CRLF;
+			entityBody = "<HTML>" + 
+				"<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+				"<BODY>404 Page Not Found</BODY></HTML>";
+		}
+
+		// Send the status line.
+		os.writeBytes(statusLine);
+
+		// Send the content type line.
+		os.writeBytes(contentTypeLine);
+
+		// Send a blank line to indicate the end of the header lines.
+		os.writeBytes(CRLF);
+		
+		// Send the entity body.
+		if (fileExists)	{
+			sendBytes(fis, os);
+			fis.close();
+		} else {
+			os.writeBytes(entityBody);
+		}
 		
 		// Close streams and socket.
 		os.close();
 		br.close();
 		socket.close();
 	}
+	
+	/*The variable, buffer, is our intermediate storage space for bytes on their way from
+	 the file to the output stream. When we read the bytes from the FileInputStream, we 
+	 check to see if read() returns minus one, indicating that the end of the file has 
+	 been reached. If the end of the file has not been reached, read() returns the number 
+	 of bytes that have been placed into buffer. We use the write() method of the
+	 OutputStream class to place these bytes into the output stream, passing to it the 
+	 name of the byte array, buffer, the starting point in the array, 0, and the number 
+	 of bytes in the array to write, bytes.*/
+	private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception
+	{
+	   // Construct a 1K buffer to hold bytes on their way to the socket.
+	   byte[] buffer = new byte[1024];
+	   int bytes = 0;
+
+	   // Copy requested file into the socket's output stream.
+	   while((bytes = fis.read(buffer)) != -1 ) {
+		  os.write(buffer, 0, bytes);
+	   }
+	}
+	
+	/*The final piece of code needed to complete the Web server is a method that will examine
+	 the extension of a file name and return a string that represents it's MIME type. If
+	  the file extension is unknown, we return the type application/octet-stream.*/
+	private static String contentType(String fileName)
+	{
+		if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+			return "text/html";
+		}
+		if(fileName.endsWith(".gif")) {
+			return "image/gif";
+		}
+		if(fileName.endsWith(".jpeg")) {
+			return "image/jpeg";
+		}
+		if(fileName.endsWith(".ram") || fileName.endsWith(".ra")) {
+			return "audio/x-pn-realaudio";
+		}
+		return "application/octet-stream";
+	}
+
 }
