b8b9360cc9c816bd81edf9bf404d1c6b3827e0e9
Alaa <awadalaa@users.noreply.github.com>
2014-01-09 22:37:23 -0500

    Update TCPClient.java

--- TCP-basics/TCPClient.java
+++ TCP-basics/TCPClient.java
@@ -7,7 +7,7 @@ class TCPClient{
 		String sentence;
 		String modifiedSentence;
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		Socket clientSocket = new Socket("192.168.1.113",6789);
+		Socket clientSocket = new Socket("hostname",6789);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader inFromServer = 
 			new BufferedReader(new InputStreamReader(
