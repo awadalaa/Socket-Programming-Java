cdcc8dc036c5535a3ea8a10bc55ab4cf896ff9de
Alaa Awad <alaa.awad.mail@gmail.com>
2014-01-09 20:58:27 -0500

    first commit

---
+++ TCP-basics/TCPClient.java
@@ -0,0 +1,23 @@
+import java.io*;
+import java.net.*;
+
+class TCPClient{
+	public static void main(String argv[]) throws Exeception
+	{
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("hostname",6789);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = 
+			new BufferdReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence  = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+	
+	}
+
+}
